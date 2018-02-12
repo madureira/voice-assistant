edc23c51e98e73520a32b722a353c59ac2d088a8
Rafael Madureira dos Santos <rafael.madureira.santos@gmail.com>
2018-02-12 14:53:55 -0200

    project restructuring

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ The goal of this project is to help people with visual impairment navigate more 
 - Node >= 8.9.4
 
 ## Main Technologies
-- [Nodejs](https://nodejs.org/)
 - [ES6](http://es6-features.org)
+- [Nodejs](https://nodejs.org/)
 - [Express](https://expressjs.com/)
 
 ## Installation
--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use('/static', express.static(__dirname + '/public'));
+app.use('/static', express.static(__dirname + '/public/assets'));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, '/public/pages/index.html'));
+});
+
+app.get('/carrinho', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/pages/carrinho.html'));
 });
 
 app.listen(3000, () => {
